8813a2d56581cc9d7a51fd65219c02f0fb305b81
Mariana Oliveira <mariana.oliveira@uniriotec.br>
2018-10-22 20:21:02 -0300

    Function Update Chart Boundaries
    Function updateChartBoundaries() it triggers when a span event is identified in the event's array. This function will update the range of the x axis. Also, was created a global variable for the chart.

--- js/script.js
+++ js/script.js
@@ -1,19 +1,19 @@
+var chart = null; //Global variable to store the chart
+
 //This function will initialize a line chart with empy data.
 
 function initializeChart() {
-  var $container = $('#line-chart'); //JQuery object that the chart will be ploted
-
   Highcharts.setOptions({
     global: {
-      useUTC: false //Set the time to local
+      useUTC: false
     }
   });
+
+  var $container = $('#line-chart');
 
   var chartingOptions = {
     chart: {
       renderTo: $container[0],
-      margin: 0,
-      height: 400,
       type: 'line'
     },
     title: {
@@ -35,10 +35,42 @@ function initializeChart() {
       align: 'right',
       verticalAlign: 'middle'
     },
-    series: []
-  }
+    series: [{
+      name: 'Linux Chrome Min Response Time', //select: ['min', 'max'] --> min
+      data: [{ //all values from the min category
+        x: 1519862400000,
+        y: 0.1
+      },
+      {
+        x: 1519862460000,
+        y: 0.2
+      }
+    ]
+  }, {
+    name: 'Linux Chrome Max Response Time', //select: ['min', 'max'] --> max
+    data: [{ //all values from the max category
+      x: 1519862400000,
+      y: 1.3
+    },
+    {
+      x: 1519862460000,
+      y: 0.9
+    }
+  ]
+}],
+responsive: {
+  rules: [{
+    condition: {
+      minWidth: 200
+    },
+    credits: {
+      enabled: false
+    }
+  }]
+}
+}
 
-  var chart = new Highcharts.Chart(chartingOptions);
+chart = new Highcharts.Chart(chartingOptions);
 };
 
 //When the document is ready, call the function to initialize the chart.
@@ -46,6 +78,13 @@ function initializeChart() {
 $( document ).ready(function() {
   initializeChart();
 });
+
+
+//This function will update de extreme values of the Chart on the x axis
+//min and max values.
+function updateChartBoundaries(min, max) {
+  chart.xAxis[0].setExtremes(min,max);
+}
 
 //This function will recover the data from the textArea, line by line, and
 //parse each string to a JSON Object.
@@ -59,20 +98,25 @@ function recoverJSONData() {
   return arrayOfEvents;
 };
 
+//This function will read one JSON Event Object at a time and
+//treat it according to its type.
+
 function readEvents() {
   var count = 0;
   var currentEvent = null;
   var arrayOfEvents = recoverJSONData();
 
   while(arrayOfEvents[count] != null) { //While there's data, still reading
-    currentEvent = arrayOfEvents[count];
-    if(currentEvent.type == "start") {
-      while(currentEvent.type != "stop") { //If it's a start event, only stops if it's a stop event
-        count++;
-        currentEvent = arrayOfEvents[count];
-        console.log(currentEvent);
-      }
-    }
+  currentEvent = arrayOfEvents[count];
+  if(currentEvent.type == "start") {
+    while(currentEvent.type != "stop") { //If it's a start event, only stops if it's a stop event
     count++;
+    currentEvent = arrayOfEvents[count];
+    if (currentEvent.type == "span") {
+      updateChartBoundaries(currentEvent.begin, currentEvent.end);
+    }
   }
+}
+count++;
+}
 }

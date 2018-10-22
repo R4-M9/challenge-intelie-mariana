402a1e55db62be0074322f9ba667e4858f187a81
Mariana Oliveira <mariana.oliveira@uniriotec.br>
2018-10-22 19:39:42 -0300

    Function Read Events and Start event
    The readEvents () function simulates a buffer read, reads one JSON event object at a time. Also, the start event was handled, getting all events below it until a stop event.

--- js/script.js
+++ js/script.js
@@ -3,10 +3,6 @@
 function initializeChart() {
   var $container = $('#line-chart'); //JQuery object that the chart will be ploted
 
-  var height= $container.height();
-  var width= $container.width();
-
-
   Highcharts.setOptions({
     global: {
       useUTC: false //Set the time to local
@@ -55,10 +51,28 @@ $( document ).ready(function() {
 //parse each string to a JSON Object.
 
 function recoverJSONData() {
-  var textArea = $('.json-data')[0].value;
+  var textArea = $('#json-data')[0].value;
   var arrayOfEvents = textArea.split("\n"); //Split the content of textarea by line
   for (i = 0; i < arrayOfEvents.length; i++) { //Transform each line in a JSON Object
     arrayOfEvents[i] = JSON.parse(arrayOfEvents[i]);
   }
   return arrayOfEvents;
 };
+
+function readEvents() {
+  var count = 0;
+  var currentEvent = null;
+  var arrayOfEvents = recoverJSONData();
+
+  while(arrayOfEvents[count] != null) { //While there's data, still reading
+    currentEvent = arrayOfEvents[count];
+    if(currentEvent.type == "start") {
+      while(currentEvent.type != "stop") { //If it's a start event, only stops if it's a stop event
+        count++;
+        currentEvent = arrayOfEvents[count];
+        console.log(currentEvent);
+      }
+    }
+    count++;
+  }
+}
